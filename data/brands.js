0aed350c8f367133281994b57fb4d2bf44a7f560
shopencercommerce-afk <shopencer.commerce@gmail.com>
2026-05-17 16:22:48 +0300

    Add vehicle brands data

---
+++ data/brands.js
@@ -0,0 +1,34 @@
+const brands = [
+  {
+    name: 'Tesla',
+    slug: 'tesla',
+    models: ['Model Y', 'Model 3', 'Model S', 'Model X']
+  },
+  {
+    name: 'BYD',
+    slug: 'byd',
+    models: ['Atto 3', 'Seal', 'Han', 'Dolphin']
+  },
+  {
+    name: 'Togg',
+    slug: 'togg',
+    models: ['T10X']
+  },
+  {
+    name: 'Chery',
+    slug: 'chery',
+    models: ['Omoda 5', 'Tiggo 7 Pro', 'Tiggo 8 Pro']
+  },
+  {
+    name: 'MG',
+    slug: 'mg',
+    models: ['MG4', 'ZS EV', 'Marvel R']
+  },
+  {
+    name: 'Skywell',
+    slug: 'skywell',
+    models: ['ET5']
+  }
+]
+
+export default brands
